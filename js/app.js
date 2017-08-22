313ecabc5c2b4cbd65bbff757f99ac63e128ac43
Akshay Guthal <akguthal@gmail.com>
2017-08-21 23:24:15 -0400

    Reworked app to use angular directives and read from json files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
-var review_app = angular.module("review-app",["ui.router"]);
+var review_app = angular.module("review-app", ["ui.router"]);
 
-review_app.controller("main-ctlr",[function(){
-    
+review_app.controller("main-ctlr", ['$scope', '$http', function($scope, $http){
+
+  loadData();
+
+
+
+  function loadData(){
+    $http.get('includes/data/articles.json').
+        success(function(data, status, headers, config) {
+          $scope.articles = data;
+        }).error(function(data, status, headers, config) {
+          console.log("Cannot read data from JSON file.");
+    });
+
+    $http.get('includes/data/months.json').
+      success(function(data, status, headers, config) {
+          $scope.months = data;
+          console.log(data);
+      }).error(function(data, status, headers, config) {
+          console.log("Cannot read data from JSON file.")
+    });
+  }
+
 }]);
+
+
+review_app.directive('article', function() {
+  return {
+    restrict: 'EA',
+    templateUrl: 'templates/article.html',
+    scope: {
+      articleData: '=articleData'
+    },
+  }
+});
+
+review_app.directive('month', function(){
+  return {
+    restrict: 'EA',
+    templateUrl: 'templates/month.html',
+    scope: {
+      id: '=monthId',
+      name: '=monthName',
+      articles: '=articles'
+    }
+  }
+});
