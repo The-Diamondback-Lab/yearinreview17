2e65f8d7e2fef5d6e4e394df222c6ba4cc27174d
sidtuli <siddytuli11@gmail.com>
2017-08-24 16:40:02 -0400

    Basic filter done

--- js/app.js
+++ js/app.js
@@ -2,14 +2,17 @@ var review_app = angular.module("review-app", []);
 
 review_app.controller("main-ctlr", ['$scope', '$http', function($scope, $http){
 
+    full_data = [];
   loadData();
-
+    
+    
 
 
   function loadData(){
     $http.get('includes/data/news.json').
         success(function(data, status, headers, config) {
           $scope.articles = data;
+            full_data = data;
         }).error(function(data, status, headers, config) {
           console.log("Cannot read data from JSON file.");
     });
@@ -22,6 +25,37 @@ review_app.controller("main-ctlr", ['$scope', '$http', function($scope, $http){
           console.log("Cannot read data from JSON file.")
     });
   }
+  
+$scope.filterCategory = function(category) {
+    if(category == "all") {
+        $scope.articles = full_data;
+    } else {
+        result = {}
+        month_keys = Object.keys(full_data)
+        for(i = 0, len = month_keys.length; i < len; i++) {
+            
+            curr_month = full_data[month_keys[i]];
+            new_month_arr = []
+            for(j = 0, month_len = curr_month.length; j < month_len; j++) {
+                curr = curr_month[j];
+                if(curr["type"].indexOf(category) >= 0 || curr["section"].indexOf(category) >= 0) {
+                    
+                    new_month_arr.push(curr);
+                }
+            }
+            
+            if(new_month_arr.length > 0) {
+                //console.log("here")
+                //console.log(month_keys[i])
+                //console.log(new_month_arr)
+                result[month_keys[i]] = new_month_arr;
+                //console.log(result)
+            }
+        }
+        $scope.articles = result; 
+    }
+}
+  
 
 }]);
 
