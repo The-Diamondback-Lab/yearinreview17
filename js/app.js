5f3dc12bbaddb1a8323ca1070952781736b33fdc
sidtuli <siddytuli11@gmail.com>
2017-08-24 20:00:31 -0400

    Basic search done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var review_app = angular.module("review-app", []);
 
 review_app.controller("main-ctlr", ['$scope', '$http', function($scope, $http){
 
+    $scope.term = "";
     full_data = [];
   loadData();
     
@@ -39,7 +40,36 @@ $scope.filterCategory = function(category) {
             for(j = 0, month_len = curr_month.length; j < month_len; j++) {
                 curr = curr_month[j];
                 if(curr["type"].indexOf(category) >= 0 || curr["section"].indexOf(category) >= 0) {
-                    
+                    new_month_arr.push(curr);
+                }
+            }
+            
+            if(new_month_arr.length > 0) {
+                //console.log("here")
+                //console.log(month_keys[i])
+                //console.log(new_month_arr)
+                result[month_keys[i]] = new_month_arr;
+                //console.log(result)
+            }
+        }
+        $scope.articles = result; 
+    }
+}
+
+$scope.search = function() {
+    term = $scope.term;
+    if(term.length < 3) {
+        $scope.articles = full_data;
+    } else {
+        result = {}
+        month_keys = Object.keys(full_data)
+        for(i = 0, len = month_keys.length; i < len; i++) {
+            
+            curr_month = full_data[month_keys[i]];
+            new_month_arr = []
+            for(j = 0, month_len = curr_month.length; j < month_len; j++) {
+                curr = curr_month[j];
+                if(curr["headline"].indexOf(term) >= 0 || curr["description"].indexOf(term) >= 0) {
                     new_month_arr.push(curr);
                 }
             }
