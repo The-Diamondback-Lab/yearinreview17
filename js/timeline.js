61818f0a7bd76fe8b9517335df4b2ef349193c29
lexusdrumgold <LexusDrumgold@gmail.com>
2017-08-26 14:55:15 -0400

    updates

--- js/timeline.js
+++ js/timeline.js
@@ -1,28 +1,24 @@
 // TIMELINE FUNCTIONS
 
-$(document).ready(function() {
+function articleTada() {
+  var rand = Math.floor(Math.random() * ('.article-block').length) + 1;
+  $('.article-block')
+      .eq(rand)
+      .addClass('is-emph')
+      .siblings().removeClass('is-emph');
+}
 
+$(document).ready(function() {
+  
+
+setInterval(function() {articleTada()}, 300);
+  
 var searchVisible = false;
 var menuVisible = false;
 
-var timeline = [
-  {month: "august 2016", id: "august16" },
-  {month: "september 2016", id: "september16" },
-  {month: "october 2016", id: "october16"},
-  {month: "november 2016", id: "november16" },
-  {month: "december 2016", id: "december16" },
-  {month: "january 2017", id: "january17" },
-  {month: "february 2017", id: "february17" },
-  {month: "march 2017", id: "march17" },
-  {month: "april 2017", id: "april17" },
-  {month: "may 2017", id: "may17" },
-  {month: "june 2017", id: "june17" },
-  {month: "july 2017", id: "july17" },
-  {month: "august 2017", id: "august17" }
-];
+
 
 //scroll to top button
-
 function scrollToTop() {
   if ($(this).scrollTop() > 300) {
     $('#top').fadeIn();
@@ -43,6 +39,7 @@ $('#top').click(function() {
   return false;
 });
 
+//search
 function searchBtn() {
     if(!searchVisible || menuVisible) {
         searchVisible = true;
@@ -58,6 +55,7 @@ $('#search-btn').click(function() {
   searchBtn();
 });
 
+//menu
 function menuBtn() {
     if(menuVisible) {
         menuVisible = false;
